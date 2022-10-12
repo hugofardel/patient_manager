ed0e9e5c308557aae4cb08b75aea64a18408f6a4
Fardel Hugo <hugo.fardel@live.fr>
2022-10-12 22:51:57 +0200

    table

--- src/views/ListPatients.js
+++ src/views/ListPatients.js
@@ -2,54 +2,75 @@ import React from "react";
 import { DataGrid } from "@mui/x-data-grid";
 
 const columns = [
-    { field: "id", headerName: "ID", minWidth: 70, flex: 0.1 },
-    { field: "firstName", headerName: "First name", minWidth: 130, flex: 0.2 },
-    { field: "lastName", headerName: "Last name", minWidth: 130, flex: 0.2 },
-    {
-        field: "age",
-        headerName: "Age",
-        type: "number",
-        minWidth: 90,
-        flex: 0.1,
-    },
-    {
-        field: "fullName",
-        headerName: "Full name",
-        description: "This column has a value getter and is not sortable.",
-        sortable: false,
-        minWidth: 160,
-        flex: 0.4,
-        valueGetter: (params) =>
-            `${params.row.firstName || ""} ${params.row.lastName || ""}`,
-    },
+	{ field: "id", headerName: "ID", minWidth: 70, flex: 0.1 },
+	{ field: "firstName", headerName: "First name", minWidth: 130, flex: 0.2 },
+	{ field: "lastName", headerName: "Last name", minWidth: 130, flex: 0.2 },
+	{
+		field: "age",
+		headerName: "Age",
+		type: "number",
+		minWidth: 90,
+		flex: 0.1,
+	},
+	{
+		field: "fullName",
+		headerName: "Full name",
+		description: "This column has a value getter and is not sortable.",
+		sortable: false,
+		minWidth: 160,
+		flex: 0.4,
+		valueGetter: (params) =>
+			`${params.row.firstName || ""} ${params.row.lastName || ""}`,
+	},
 ];
 
 const rows = [
-    { id: 1, lastName: "Snow", firstName: "Jon", age: 35 },
-    { id: 2, lastName: "Lannister", firstName: "Cersei", age: 42 },
-    { id: 3, lastName: "Lannister", firstName: "Jaime", age: 45 },
-    { id: 4, lastName: "Stark", firstName: "Arya", age: 16 },
-    { id: 5, lastName: "Targaryen", firstName: "Daenerys", age: null },
-    { id: 6, lastName: "Melisandre", firstName: null, age: 150 },
-    { id: 7, lastName: "Clifford", firstName: "Ferrara", age: 44 },
-    { id: 8, lastName: "Frances", firstName: "Rossini", age: 36 },
-    { id: 9, lastName: "Roxie", firstName: "Harvey", age: 65 },
+	{ id: 1, lastName: "Snow", firstName: "Jon", age: 35 },
+	{ id: 2, lastName: "Lannister", firstName: "Cersei", age: 42 },
+	{ id: 3, lastName: "Lannister", firstName: "Jaime", age: 45 },
+	{ id: 4, lastName: "Stark", firstName: "Arya", age: 16 },
+	{ id: 5, lastName: "Targaryen", firstName: "Daenerys", age: null },
+	{ id: 6, lastName: "Melisandre", firstName: null, age: 150 },
+	{ id: 7, lastName: "Clifford", firstName: "Ferrara", age: 44 },
+	{ id: 8, lastName: "Frances", firstName: "Rossini", age: 36 },
+	{ id: 9, lastName: "Roxie", firstName: "Harvey", age: 65 },
 ];
 
 const ListPatients = () => {
-    return (
-        <div className="page-list-patients">
-            <div style={{ height: 400, width: "100%" }}>
-                <DataGrid
-                    rows={rows}
-                    columns={columns}
-                    pageSize={5}
-                    rowsPerPageOptions={[5]}
-                    checkboxSelection
-                />
-            </div>
-        </div>
-    );
+	return (
+		<div className="page-list-patients">
+			<div style={{ height: 400, width: "100%" }}>
+				<DataGrid
+					rows={rows}
+					columns={columns}
+					pageSize={5}
+					rowsPerPageOptions={[5]}
+					checkboxSelection
+				/>
+			</div>
+
+			<table>
+				<thead>
+					<tr>
+						<th>LastName</th>
+						<th>FirstName</th>
+						<th>Age</th>
+					</tr>
+				</thead>
+				<tbody>
+					{rows.map((val, key) => {
+						return (
+							<tr key={key}>
+								<td>{val.lastName}</td>
+								<td>{val.firstName}</td>
+								<td>{val.age}</td>
+							</tr>
+						);
+					})}
+				</tbody>
+			</table>
+		</div>
+	);
 };
 
 export default ListPatients;
